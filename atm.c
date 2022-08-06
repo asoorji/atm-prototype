200b0ae342a2c60b0a95c69484d694b2590d0e16
Olukayode Ifeoluwa Joshua <ifeoluwajoshua2015@gmail.com>
2022-08-06 01:18:43 +0100

    pin checked

--- atm.c
+++ atm.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
+#include<stdbool.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<time.h>
+
 
 int main () {
 
+char name[150];
 double balance = 5000;
 int pin;
 int tranx;
@@ -10,34 +17,43 @@ int debit;
 int credit;
 int new_balance;
 
-printf("Enter Pin: ");
-scanf("%d", &pin);
+printf("\nRegister your name here : ");
+fgets(name, 150, stdin);
+name[strlen(name)-1]= '\0';
+while(strlen(name) == 0){
+printf("\nPlease Register you name: ");
+fgets(name, 150, stdin);
+name[strlen(name)-1]= '\0';
+}
+printf("\nWelcome %s. You will love this adventure.\n", name);
 
+printf("\nEnter Pin: ");
+scanf("%d", &pin);
+while(pin != 1234){
+printf("\nplease Re-Enter Pin: ");
+scanf("%d\n", &pin);
+}
+printf("\n%d is the correct pin\n", pin);
 if(pin == 1234){
-   printf("Welcome to the ATM\n");
-   printf("Which transaction do you want to perform\n? :");
-    printf("Enter 1 for debit || 2 for credit: ");
+   printf("\nWelcome to the ATM\n");
+   printf("\nFor withdrawal enter (1)\nFor payment enter (2)");
+   printf("\nEnter your choice : ");
    scanf("%d", &tranx);
    if(tranx == 1){
-   printf("How much do you want to withdraw?: ");
+   printf("\nHow much do you want to withdraw?: ");
    scanf("%d", &debit);
    new_balance = balance - debit;
-   printf("You just withdrew %d\n", debit);
-   printf("Your balance is %d", new_balance);
+   printf("\nYou just withdrew %d\n", debit);
+   printf("\nYour balance is %d", new_balance);
    }
    else {
-   printf("How much do you want to deposit?: ");
+   printf("\nHow much do you want to deposit?: ");
    scanf("%d", &credit);
    new_balance = balance + credit;
-   printf("You just deposited %d\n", credit);
-   printf("Your balance is %d", new_balance);
+   printf("\nYou just deposited %d\n", credit);
+   printf("\nYour balance is %d\n", new_balance);
    }
 }
-else {
-   printf("Please enter a valid pin: ");
-   scanf("%d", &tranx);
-}
-
    return 0;
    
 };
